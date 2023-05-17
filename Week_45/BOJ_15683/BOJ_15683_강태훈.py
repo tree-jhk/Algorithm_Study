c5030032795e86028ca5de82e0dbefbdbd8525ff
KTH <thkyang324@kakao.com>
2023-05-18 08:54:04 +0900

    강태훈 15683

--- Week_45/BOJ_15683/BOJ_15683_강태훈.py
+++ Week_45/BOJ_15683/BOJ_15683_강태훈.py
@@ -17,12 +17,11 @@ class Mover:
         self.dx = (0,1,0,-1)
         self.dy = (-1,0,1,0)
 
-    def __call__(self, x, y, direction, value=0):
-        while True:
-            nx, ny = x+value*self.dx[direction], y+value*self.dy[direction]
+    def __call__(self, x, y, direction):
+        for i in range(8):
+            nx, ny = x+i*self.dx[direction], y+i*self.dy[direction]
             if any([nx<0, ny<0, nx>=m, ny>=n, (nx,ny) in self.walls]):
                 break # 벽이나 보드의 너비, 높이에 의해 시야각이 끊긴경우 break
-            value += 1
             yield nx, ny # cctv가 볼 수 있는 좌표 yield
 
 def turn(view:list, turnval:int):
@@ -62,3 +61,79 @@ if __name__ =="__main__":
             else: # 빈칸은 0에, cctv는 1에 저장
                 info[min(1, v)].add((x,y))
     print(solve(board, info, n, m))
+
+# # 2. 누적합 시간초과
+# import sys
+# from itertools import product, accumulate
+# input = lambda : map(int, sys.stdin.readline().split())
+# # cctv별 볼 수 있는 방향, 0123은 상우하좌
+# cctv = [[],[1],[1,3],[0,1],[0,1,3],[0,1,2,3]]
+
+# class Mover:
+#     """
+#     입력받은 n, m, walls, dx, dy를 고려
+#     호출 시 현재 좌표에서 어떠한 방향으로 볼 수 있는 모든 좌표를 yield
+#     """
+#     __slots__=['n', 'm', 'walls', 'dx', 'dy']
+#     def __init__(self, n, m, walls):
+#         self.n = n
+#         self.m = m
+#         self.walls = walls
+#         self.dx = (0,1,0,-1)
+#         self.dy = (-1,0,1,0)
+
+#     def __call__(self, x, y, direction, cnt=-1):
+#         answer = None
+#         for i in range(8):
+#             nx, ny = x+i*self.dx[direction], y+i*self.dy[direction]
+#             if any([nx<0, ny<0, nx>=m, ny>=n, (nx,ny) in self.walls]):
+#                 return answer
+#             answer = [nx, ny]
+#         return answer
+    
+# def turn(view:list, turnval:int):
+#     """ cctv의 방향(view)을 회전량(turnval)만큼 돌림 """
+#     for i in view:
+#         yield (i+turnval)%4
+
+# def solve(board, info, n, m):
+#     move = Mover(n, m, info[6])
+#     answer = float("inf")
+#     for case in product(range(4), repeat=len(info[1])): # cctv가 볼 수 있는 모든 방향에 대하여
+#         field = [[0]*(m+1) for _ in range(n+1)]
+#         for (x, y), cctv_turn_value in zip(info[1], case):
+#             """ 이 부분 누적합으로 가능해 보임. 안해도 느리지만 통과는 됨. """
+#             for cctv_dir in turn(cctv[board[y][x]], cctv_turn_value):
+#                 _x, _y = move(x, y, cctv_dir)
+#                 xmin, xmax = sorted([x, _x])
+#                 ymin, ymax = sorted([y, _y])
+#                 field[ymin][xmin] += 1
+#                 field[ymax+1][xmax+1] += 1
+#                 field[ymin][xmax+1] -= 1
+#                 field[ymax+1][xmin] -= 1
+#         field = [list(accumulate(i)) for i in field]
+#         for x in range(m):
+#             for y in range(1, n):
+#                 field[y][x] += field[y-1][x]
+#         for x, y in info[6]:
+#             field[y][x] += 1
+#         cnt = 0
+#         for y in range(n):
+#             for x in range(m):
+#                 if field[y][x]==0:
+#                     cnt += 1
+#         answer = min(answer, cnt)
+#     return answer
+
+# if __name__ =="__main__":
+#     n, m = input()
+#     board = [[0]*m for _ in range(n)]
+#     info = {i:set() for i in (0,1,6)}
+#     for y in range(n):
+#         for x, v in enumerate(input()):
+#             board[y][x] = v
+#             if v==6:
+#                 info[6].add((x,y))
+#             else:
+#                 info[min(1, v)].add((x,y))
+#     print(solve(board, info, n, m))
