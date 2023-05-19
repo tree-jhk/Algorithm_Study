31c1789dddf7bedd82f3d474576e4c602a4d4092
KTH <thkyang324@kakao.com>
2023-05-20 03:28:19 +0900

    강태훈 동전뒤집기

---
+++ Week_46/BOJ_1285/BOJ_1285_강태훈.py
@@ -0,0 +1,32 @@
+import sys
+from collections import Counter
+input = lambda : sys.stdin.readline().rstrip()
+
+def str2bin(str, l=0):
+    for token in str:
+        l<<=1
+        if token=="H":
+            l+=1
+    return l
+
+def count_1(num, cnt=0):
+    while num:
+        cnt += num&1
+        num>>=1
+    return cnt
+
+def main(o_board, n, answer=float("inf")):
+    board = Counter(o_board)
+    for mask in range(1<<n):
+        local = 0
+        for line, size in board.items():
+            cnt = count_1(line^mask)
+            local += min(cnt, n-cnt)*size
+        answer = min(answer, local)
+    return answer
+
+if __name__ == "__main__":
+    n = int(input())
+    board = [str2bin(input()) for _ in range(n)]
+    print(main(board, n))
+    
